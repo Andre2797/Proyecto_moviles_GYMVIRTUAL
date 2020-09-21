98c067cc6748c5c791c3556b317e097c726afc0a
andrea <as7moc@hotmail.com>
2020-09-21 08:30:54 -0500

    menu_principal_terminado

---
+++ GymVirtualAndroid/app/src/main/java/com/example/gymvirtual/OnCalendario.kt
@@ -0,0 +1,5 @@
+package com.example.gymvirtual
+
+interface OnCalendario{
+    fun onCalendarioItemClick(position:Int)
+}
--- GymVirtualAndroid/app/src/main/java/com/example/gymvirtual/RecyclerAdaptador.kt
+++ GymVirtualAndroid/app/src/main/java/com/example/gymvirtual/RecyclerAdaptador.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class RecyclerAdaptador(private val listaretos: List<Retos>,
-                        private val contexto: Context?,
-                        private val recyclerView: RecyclerView
+class RecyclerAdaptador(
+    var contexto : Context,
+    var titulos : ArrayList<String>,
+    var imagenes : ArrayList<String>,
+    var onCalendario: OnCalendario
 ):RecyclerView.Adapter<RecyclerAdaptador.MyViewHolder>() {
 
     lateinit var model:ArrayList<Retos>
@@ -19,31 +24,46 @@ class RecyclerAdaptador(private val listaretos: List<Retos>,
     inner class MyViewHolder(view: View) :
         androidx.recyclerview.widget.RecyclerView.ViewHolder(view) {
         var titulo: TextView
-        var descripcion: TextView
+
         var imagen:ImageView
+        var parentLayout:RelativeLayout= view.findViewById(R.id.id_relativeLayout)
+
         init {
             titulo = view.findViewById(R.id.titulo)
-            descripcion = view.findViewById(R.id.descripcion)
+
             imagen = view.findViewById(R.id.id_abdomen)
+            view.setOnClickListener{
+                val position:Int=adapterPosition
+
+            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView= LayoutInflater.from(parent.context)
-            .inflate(R.layout.rv_retos,parent,false)// recursos del adaptador
-        itemView.setOnClickListener(this.lsitener)
+            .inflate(R.layout.rv_retos,parent,false)
+
         return  MyViewHolder(itemView)
     }
 
     override fun getItemCount(): Int {
-        return model.size
+        return imagenes.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
        //posision
-             val retos=listaretos[position]
-           holder.titulo.text =retos.titulo
-           holder.descripcion.text = retos.descripcion
-          holder.imagen.setImageResource(retos.imagen)
+        // val retos=listaretos[position]
+        // holder.titulo.text =retos.titulo
+        // holder.descripcion.text = retos.descripcion
+        // holder.imagen.setImageResource(retos.imagen)
+        Glide.with(contexto)
+            .asBitmap()
+            .load(imagenes[position])
+            .into(holder.imagen)
+
+        holder.titulo.setText(titulos[position])
+        holder.parentLayout.setOnClickListener {
+            onCalendario.onCalendarioItemClick(position)
+        }
     }
 }
